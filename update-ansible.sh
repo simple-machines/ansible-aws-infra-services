ab91815874f6741f7675c1b7abb0f97c1cfd269c
simplesteph <stephane.maarek@gmail.com>
2017-04-05 12:43:23 +1000

    fixed update-ansible script to point against master

--- update-ansible.sh
+++ update-ansible.sh
@@ -6,7 +6,7 @@ if [ ! -z "$1" ]
     UPDATE_FROM=$1
   else
     echo "Updating from github"
-    VERSION=custom-role
+    VERSION=master
     rm -Rf ansible-aws-infra-services-$VERSION
     curl -L --silent --show-error https://github.com/simple-machines/ansible-aws-infra-services/archive/$VERSION.tar.gz | tar xz
     UPDATE_FROM="ansible-aws-infra-services-$VERSION"
