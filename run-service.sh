17c2c63ab03bc6ac1c7486376bd38913f34267af
Thomas Sutton <me@thomas-sutton.id.au>
2017-04-28 14:59:53 +1000

    Shellcheck Episode Two: Attack of the quotes

--- run-service.sh
+++ run-service.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
+#
+# NAME
+#     run-service.sh - Run Ansible with service configurations
+#
+# SYNOPSIS
+#     ./run-service.sh CLUSTER SERVICE ENV
+
+set -eu -o pipefail
+
 source ANSIBLE_DOCKER_ENV
 
-docker run -it -v "$(pwd):/project" \
+USAGE=$(sed -E -e '/^$/q' -e 's/^#($|!.*| (.*))$/\2/' "$0")
+
+docker run -it -v "${PWD}:/project" \
                 -v ~/.aws:/root/.aws \
-                -e CLUSTER_NAME=$1 \
-                -e SERVICE_NAME=$2 \
-                -e ENV=$3 \
-                simplemachines/ansible-template:$DOCKER_TAG \
+                -e "CLUSTER_NAME=${1:?"Required argument missing. $USAGE"}" \
+                -e "SERVICE_NAME=${2:?"Required argument missing. $USAGE"}" \
+                -e "ENV=${3:?"Required argument missing. $USAGE"}" \
+                "simplemachines/ansible-template:${DOCKER_TAG:?"Required variable missing. $USAGE"}" \
                 scripts/run-service.sh
